73c4acb0ec32699d2f4959478d4e6af213194478
Akhilarappura <itsmeakhilarappura@gmail.com>
2024-07-05 16:42:51 +0530

    uI.1.commit

--- server/controller/user/checkoutController.js
+++ server/controller/user/checkoutController.js
@@ -9,10 +9,12 @@ const walletdb = require('../../model/walletModel');
 const categorydb = require('../../model/categoryModel')
 
 
+
+
 const razorpay = new Razorpay({
-    key_id: 'rzp_test_Yf4QX1fH3V3BDE',
-    key_secret: 'H265FfTj4892G8z94yh4vSs3'
-})
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET
+});
 
 
 
