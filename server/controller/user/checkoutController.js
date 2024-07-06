903ad45d761340f32daa712eb79791cc04e5039a
Akhilarappura <itsmeakhilarappura@gmail.com>
2024-07-06 12:18:53 +0530

    key

--- server/controller/user/checkoutController.js
+++ server/controller/user/checkoutController.js
@@ -12,8 +12,8 @@ const categorydb = require('../../model/categoryModel')
 
 
 const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET
+    key_id: 'rzp_test_Yf4QX1fH3V3BDE',
+    key_secret: 'H265FfTj4892G8z94yh4vSs3'
 });
 
 
